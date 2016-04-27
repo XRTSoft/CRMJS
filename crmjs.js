950e560687cd8d991bba260a1c21a8e68cc09601
Robert Tilling <robert.tilling@googlemail.com>
2016-04-27 21:01:00 +0100

    Restructured
    Changed from module pattern as I found this easier to use on CRM form
    scripts.

--- crmjs.js
+++ crmjs.js
@@ -1,73 +1,92 @@
-var crmjs = (function (xrm, errorOnNotFound) {
-    var xrmvar = xrm;
-    if (xrmvar === null) {
-        if (typeof Xrm !== 'undefined' && Xrm != null && Xrm.Page != null && Xrm.Page.context != null) {
-            scriptContext = Xrm.Page.context;
-        } else {
-            //To do - load ClientGlobalContext
-        }
-    }
-    var throwErrorOnNotFound = errorOnNotFound;
-    throwErrorOnNotFound = IsNullOrUndefined(throwErrorOnNotFound) ? false : throwErrorOnNotFound;
-    
-    function IsNullOrUndefined(param) {
-        return param === null || param === undefined;
-    }
+var crmjs = {
+    xrmvar: Xrm,
+    throwErrorOnNotFound: true,
+    FORM_CREATE: 1,
+    FORM_UPDATE: 2,
+    FORM_READ_ONLY: 3,
+    FORM_DISABLED: 4,
+    AUTOSAVE: 70,
 
-    return {
-        attr: {
-            get: function (fieldName) {
-                var attr = xrmvar.Page.getAttribute(fieldName);
-                if (throwErrorOnNotFound) {
-                    throw 'attributes.get - no attribute found with name: ' + fieldName;
-                }
-                return attr;
-            },
-            getControl: function (fieldName) {
-                var attr = xrmvar.Page.getControl(fieldName);
-                if (throwErrorOnNotFound) {
-                    throw 'attributes.getControl - no attribute found with name: ' + fieldName;
-                }
-                return attr;
-            },
-            getValue: function (fieldName) {
-                var attr = crmjs.attr.get(fieldName);
-                return attr.getValue();
-            },
-            setValue: function(fieldName, value) {
-                var attr = crmjs.attr.get(fieldName);
-                try {
-                    attr.setValue(value);
-                } catch (ex) {
-                    if (throwErrorOnNotFound) {
-                        throw 'attr.setValue (' + fieldName + ') - Exception: ' + ex.message;
-                    } else {
-                        throw ex;
-                    }
-                }
-            },
-            setLookupValue: function (fieldName, lookupId, lookupName, lookupType) {
-                var attr = crmjs.attr.get(fieldName);
-                try {
-                    var array = [];
-                    array[0] = {};
-                    array[0].id = lookupId;
-                    array[0].name = lookupName;
-                    array[0].entityType = lookupType;
-                    attr.setValue(array);
-                } catch (ex) {
-                    if (throwErrorOnNotFound) {
-                        throw 'attr.setLookupValue ('+ fieldName +') - Exception: ' + ex.message;
-                    } else {
-                        throw ex;
-                    }
+    isNullOrUndefined: function (param) {
+        return param === null || param === undefined;
+    },
+    form: {
+        name: function () {
+            return crmjs.xrmvar.Page.ui.formSelector.getCurrentItem() === null ? '' : crmjs.xrmvar.Page.ui.formSelector.getCurrentItem().getLabel();
+        },
+        entityName: function () {
+            return crmjs.xrmvar.Page.data.entity.getEntityName();
+        },
+        getId: function () {
+            return crmjs.xrmvar.Page.data.entity.getId();
+        }
+    },
+    notifications: {
+        clearField: function (field) {
+            var control = crmjs.attr.getControl();
+            control.clearNotification();
+        },
+        setField: function (field, msg) {
+            var control = crmjs.attr.getControl();
+            control.setNotification(msg);
+        },
+        clearForm: function (id) {
+            crmjs.xrmvar.Page.ui.clearFormNotification(id);
+        },
+        setForm: function (field, msg, level) {
+            if (level !== 'ERROR' && level !== 'WARNING' && level !== 'INFORMATION') {
+                leve = 'INFORMATION';
+            }
+            crmjs.xrmvar.Page.ui.setFormNotification(message, level, id);
+        }
+    },
+    attr: {
+        get: function (fieldName) {
+            var attr = crmjs.xrmvar.Page.getAttribute(fieldName);
+            if (attr === null && crmjs.throwErrorOnNotFound) {
+                throw 'attributes.get - no attribute found with name: ' + fieldName;
+            }
+            return attr;
+        },
+        getControl: function (fieldName) {
+            var attr = crmjs.xrmvar.Page.getControl(fieldName);
+            if (attr === null && crmjs.throwErrorOnNotFound) {
+                throw 'attributes.getControl - no attribute found with name: ' + fieldName;
+            }
+            return attr;
+        },
+        getValue: function (fieldName) {
+            var attr = crmjs.attr.get(fieldName);
+            return attr.getValue();
+        },
+        setValue: function (fieldName, value) {
+            var attr = crmjs.attr.get(fieldName);
+            try {
+                attr.setValue(value);
+            } catch (ex) {
+                if (crmjs.throwErrorOnNotFound) {
+                    throw 'attr.setValue (' + fieldName + ') - Exception: ' + ex.message;
+                } else {
+                    throw ex;
                 }
             }
         },
-        form: {
-            getId: function() {
-                return xrmvar.Page.data.entity.getId();
+        setLookupValue: function (fieldName, lookupId, lookupName, lookupType) {
+            var attr = crmjs.attr.get(fieldName);
+            try {
+                var array = [];
+                array[0] = {};
+                array[0].id = lookupId;
+                array[0].name = lookupName;
+                array[0].entityType = lookupType;
+                attr.setValue(array);
+            } catch (ex) {
+                if (crmjs.throwErrorOnNotFound) {
+                    throw 'attr.setLookupValue (' + fieldName + ') - Exception: ' + ex.message;
+                } else {
+                    throw ex;
+                }
             }
         }
     }
-})();
+}
