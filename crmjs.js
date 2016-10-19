9e60b730b4d3982ec07a79a759387b15c3d17d0f
Robert Tilling <robert.tilling@googlemail.com>
2016-10-19 21:14:34 +0100

    Oct 2016 update
    Fixed getControl helpers. Added helpers for WebAPI.

--- crmjs.js
+++ crmjs.js
@@ -1,92 +1,365 @@
-var crmjs = {
-    xrmvar: Xrm,
-    throwErrorOnNotFound: true,
-    FORM_CREATE: 1,
-    FORM_UPDATE: 2,
-    FORM_READ_ONLY: 3,
-    FORM_DISABLED: 4,
-    AUTOSAVE: 70,
+var crmjs = (function () {
+    var xrmvar = Xrm;
+    var throwErrorOnNotFound = true;
 
-    isNullOrUndefined: function (param) {
+
+    function IsNullOrUndefined(param) {
         return param === null || param === undefined;
-    },
-    form: {
-        name: function () {
-            return crmjs.xrmvar.Page.ui.formSelector.getCurrentItem() === null ? '' : crmjs.xrmvar.Page.ui.formSelector.getCurrentItem().getLabel();
+    }
+    function IsNullOrUndefinedOrEmptyString(param) {
+        var isEmpty = IsNullOrUndefined(param);
+        return isEmpty ? isEmpty : isEmpty === '';
+    }
+
+    var FORM_TYPE_CREATE = 1;
+    var FORM_TYPE_UPDATE = 2;
+    var FORM_TYPE_READ_ONLY = 3;
+    var FORM_TYPE_DISABLED = 4;
+    var FORM_TYPE_BULK_EDIT = 6;
+    var FORM_TYPE_READ_OPTIMIZED = 11;
+
+    return {
+        FORM_TYPE_CREATE: 1,
+        FORM_TYPE_UPDATE: 2,
+        FORM_TYPE_READ_ONLY: 3,
+        FORM_TYPE_DISABLED: 4,
+        FORM_TYPE_BULK_EDIT: 6,
+        FORM_TYPE_READ_OPTIMIZED: 11,
+        isNullOrUndefined: function (param) {
+            return param === null || param === undefined;
         },
-        entityName: function () {
-            return crmjs.xrmvar.Page.data.entity.getEntityName();
+        isNullOrUndefinedOrEmptyString: function (param) {
+            var isEmpty = IsNullOrUndefined(param);
+            return isEmpty ? isEmpty : isEmpty === '';
         },
-        getId: function () {
-            return crmjs.xrmvar.Page.data.entity.getId();
-        }
-    },
-    notifications: {
-        clearField: function (field) {
-            var control = crmjs.attr.getControl();
-            control.clearNotification();
+        init: function () {
+
         },
-        setField: function (field, msg) {
-            var control = crmjs.attr.getControl();
-            control.setNotification(msg);
-        },
-        clearForm: function (id) {
-            crmjs.xrmvar.Page.ui.clearFormNotification(id);
-        },
-        setForm: function (field, msg, level) {
-            if (level !== 'ERROR' && level !== 'WARNING' && level !== 'INFORMATION') {
-                leve = 'INFORMATION';
+        webAPI: {
+            fixId: function (id) {
+                id = crmjs.isNullOrUndefined(id) ? '' : id;
+                id = id.replace('{', '');
+                id = id.replace('}', '');
+                return id;
+            },
+            getByUrl: function (url, onComplete) {
+                var xhr = new XMLHttpRequest()
+                xhr.open('get', '/api/data/v8.1/adx_sitesettings?$filter=contains(adx_name,%27Services/TipPermits/DefaultPermitAllocation%27)&$select=adx_value', true);
+                xhr.setRequestHeader('OData-MaxVersion', '4.0');
+                xhr.setRequestHeader('OData-Version', '4.0');
+                xhr.setRequestHeader('Prefer', 'odata.include-annotations="*"');
+                xhr.onreadystatechange = function (result) {
+                    if (this.readyState == 4) {
+                        xhr.onreadystatechange = null;
+                        if (onComplete !== null && typeof onComplete === 'function') {
+                            onComplete(JSON.parse(this.responseText));
+                        }
+                    }
+                };
+
+                //Send XHRs
+                xhr.send();
+            },
+            filter: function (setName, filter, select, onComplete) {
+                var url = '/api/data/v8.1/' + setName + '/';
+                url += '?filter=' + filter + '&';
+                url += '$select=' + select;
+                crmjs.webAPI.getByUrl(url, onComplete);
+            },
+            byId: function (setName, id, select, onComplete) {
+                var url = '/api/data/v8.1/' + setName + '(' + crmjs.webAPI.fixId(id) + ')';
+                url += '$select=' + select;
+                crmjs.webAPI.getByUrl(url, onComplete);
             }
-            crmjs.xrmvar.Page.ui.setFormNotification(message, level, id);
-        }
-    },
-    attr: {
-        get: function (fieldName) {
-            var attr = crmjs.xrmvar.Page.getAttribute(fieldName);
-            if (attr === null && crmjs.throwErrorOnNotFound) {
-                throw 'attributes.get - no attribute found with name: ' + fieldName;
-            }
-            return attr;
         },
-        getControl: function (fieldName) {
-            var attr = crmjs.xrmvar.Page.getControl(fieldName);
-            if (attr === null && crmjs.throwErrorOnNotFound) {
-                throw 'attributes.getControl - no attribute found with name: ' + fieldName;
-            }
-            return attr;
-        },
-        getValue: function (fieldName) {
-            var attr = crmjs.attr.get(fieldName);
-            return attr.getValue();
-        },
-        setValue: function (fieldName, value) {
-            var attr = crmjs.attr.get(fieldName);
-            try {
-                attr.setValue(value);
-            } catch (ex) {
-                if (crmjs.throwErrorOnNotFound) {
-                    throw 'attr.setValue (' + fieldName + ') - Exception: ' + ex.message;
-                } else {
-                    throw ex;
+        attr: {
+            get: function (fieldName) {
+                var attr = xrmvar.Page.getAttribute(fieldName);
+                if (throwErrorOnNotFound && IsNullOrUndefined(attr)) {
+                    throw 'attributes.get - no attribute found with name: ' + fieldName;
                 }
+                return attr;
+            },
+            getControl: function (fieldName) {
+                var attr = xrmvar.Page.getControl(fieldName);
+                if (throwErrorOnNotFound && IsNullOrUndefined(attr)) {
+                    throw 'attributes.getControl - no control found with name: ' + fieldName;
+                }
+                return attr;
+            },
+            getValue: function (fieldName) {
+                var attr = crmjs.attr.get(fieldName);
+                return attr.getValue();
+            },
+            getCurrentValue: function (fieldName) {
+                var control = crmjs.attr.getControl(fieldName);
+                return control.getValue();
+            },
+            setValue: function (fieldName, value) {
+                var attr = crmjs.attr.get(fieldName);
+                try {
+                    attr.setValue(value);
+                } catch (ex) {
+                    if (throwErrorOnNotFound) {
+                        throw 'attr.setValue (' + fieldName + ') - Exception: ' + ex.message;
+                    } else {
+                        throw ex;
+                    }
+                }
+            },
+            setLookupValue: function (fieldName, lookupId, lookupName, lookupType) {
+                var attr = crmjs.attr.get(fieldName);
+                try {
+                    var array = [];
+                    array[0] = {};
+                    array[0].id = lookupId;
+                    array[0].name = lookupName;
+                    array[0].entityType = lookupType;
+                    attr.setValue(array);
+                } catch (ex) {
+                    if (throwErrorOnNotFound) {
+                        throw 'attr.setLookupValue (' + fieldName + ') - Exception: ' + ex.message;
+                    } else {
+                        throw ex;
+                    }
+                }
+            },
+            getDisabled: function (fieldName) {
+                var control = crmjs.attr.getControl(fieldName);
+                return control.getDisabled();
+            },
+            setDisabled: function (fieldName, isDisabled) {
+                var control = crmjs.attr.getControl(fieldName);
+                control.setDisabled(isDisabled);
+            },
+            getVisible: function (fieldName) {
+                var control = crmjs.attr.getControl(fieldName);
+                return control.getVisible();
+            },
+            setVisible: function (fieldName, isDisabled) {
+                var control = crmjs.attr.getControl(fieldName);
+                control.setVisible(isDisabled);
+            },
+            getLabel: function (fieldName) {
+                var control = crmjs.attr.getControl(fieldName);
+                return control.getLabel();
+            },
+            setLabel: function (fieldName, label) {
+                var control = crmjs.attr.getControl(fieldName);
+                control.setLabel(label);
             }
         },
-        setLookupValue: function (fieldName, lookupId, lookupName, lookupType) {
-            var attr = crmjs.attr.get(fieldName);
-            try {
-                var array = [];
-                array[0] = {};
-                array[0].id = lookupId;
-                array[0].name = lookupName;
-                array[0].entityType = lookupType;
-                attr.setValue(array);
-            } catch (ex) {
-                if (crmjs.throwErrorOnNotFound) {
-                    throw 'attr.setLookupValue (' + fieldName + ') - Exception: ' + ex.message;
-                } else {
-                    throw ex;
+        grids: {
+            refresh: function (gridName) {
+                var control = crmjs.attr.getControl(gridName);
+                control.refresh();
+            },
+            get: function (gridName) {
+                var control = crmjs.attr.getControl(gridName);
+                return control.getGrid();
+            },
+            getEntityName: function (gridName) {
+                var control = crmjs.attr.getControl(gridName);
+                return control.getEntityName();
+            },
+            addOnLoad: function (gridName, onLoadFunc) {
+                var control = crmjs.attr.getControl(gridName);
+                control.addOnLoad(onLoadFunc);
+            }
+        },
+        form: {
+            getLabel: function () {
+                var form = xrmvar.Page.ui.formSelector.getCurrentItem();
+                return form === null ? '' : form.getLabel();
+            },
+            getEntityId: function () {
+                return xrmvar.Page.data.entity.getId();
+            },
+            getEntityName: function () {
+                return xrmvar.Page.data.entity.getEntityName();
+            },
+            getFormType: function () {
+                return xrmvar.Page.ui.getFormType();
+            },
+            addOnSave: function (onSaveFunc) {
+                xrmvar.Page.data.entity.addOnSave(onSaveFunc);
+            },
+            removeOnSave: function (onSaveFunc) {
+                xrmvar.Page.data.entity.removeOnSave(onSaveFunc);
+            },
+            close: function () {
+                xrmvr.Page.ui.close();
+            },
+            refreshRibbon: function () {
+                xrmvar.Page.ui.refreshRibbon();
+            },
+            quickForm: {
+                get: function (formName) {
+                    var form = xrmvar.Page.ui.quickForm.get(formName);
+                    if (throwErrorOnNotFound && IsNullOrUndefined(form)) {
+                        throw 'attributes.get - no quickview form found with name: ' + formName;
+                    }
+                    return form;
+                },
+                getWhenLoaded: function (formName) {
+                    var form = xrmvar.form.quickForm.get(formName);
+                    if (form.isLoaded()) {
+                        return form;
+                    }
+                    window.setTimeout(xrmvar.forms.quickForm.get(formName), 100);
                 }
+            },
+            tab: {
+                get: function (tabName) {
+                    var tab = xrmvar.Page.ui.tabs.get(tabName);
+                    if (throwErrorOnNotFound && IsNullOrUndefined(tab)) {
+                        throw 'form.getTab - no tab found with name: ' + tabName;
+                    }
+                    return tab;
+                },
+                isCollapsed: function (tabName) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.getDisplayState() === 'collapsed';
+                },
+                collapse: function (tabName) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.setDisplayState('collapsed');
+                },
+                expand: function (tabName) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.setDisplayState('expanded');
+                },
+                getLabel: function (tabName) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.getLabel();
+                },
+                setLabel: function (tabName, label) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.setLabel(label);
+                },
+                getVisible: function (tabName) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.getVisible();
+                },
+                setVisible: function (tabName, isVisible) {
+                    var tab = crmjs.form.tab.get(tabName);
+                    return tab.setVisible(isVisible);
+                },
+            },
+            section: {
+                get: function (sectionName) {
+                    var tabs = xrmvar.Page.ui.tabs.getAll();
+                    if (tabs === null || tabs.length < 1) {
+                        if (throwErrorOnNotFound) {
+                            throw 'form.getSection - no tabs found on form to use to find section: ' + sectionName;
+                        }
+                    }
+
+                    var section = null;
+                    for (var ctr = 0; ctr < tabs.length; ctr++) {
+                        section = tabs[ctr].sections.getByName(sectionName);
+                        if (section !== null) {
+                            break;
+                        }
+                    }
+                    if (throwErrorOnNotFound && IsNullOrUndefined(section)) {
+                        throw 'form.getSection - no section found with name: ' + sectionName;
+                    }
+                    return section;
+                },
+                getLabel: function (sectionName) {
+                    var section = crmjs.form.section.get(sectionName);
+                    return section.getLabel();
+                },
+                setLabel: function (sectionName, label) {
+                    var section = crmjs.form.section.get(sectionName);
+                    return section.setLabel(label);
+                },
+                getVisible: function (sectionName) {
+                    var section = crmjs.form.section.get(sectionName);
+                    return section.getVisible();
+                },
+                setVisible: function (sectionName, isVisible) {
+                    var section = crmjs.form.section.get(sectionName);
+                    return section.setVisible(isVisible);
+                },
+            }
+        },
+        navItems: {
+            get: function (navName) {
+                var nav = xrmvar.Page.ui.navigation.items(navName);
+                if (throwErrorOnNotFound && IsNullOrUndefined(nav)) {
+                    throw 'navItems.get - no nav item found with name: ' + navName;
+                }
+                return nav;
+            },
+            setLabel: function (navName, label) {
+                var navItem = crmjs.navItems.get(navName);
+                navItem.setLabel(label);
+            },
+            getVisible: function (navName) {
+                var navItem = crmjs.navItems.get(navName);
+                navItem.getVisible();
+            },
+            setVisible: function (navName, isVisible) {
+                var navItem = crmjs.navItems.get(navName);
+                navItem.setVisible(isVisible);
+            }
+        },
+        events: {
+            refresh: function (doSave, successFunc, errorFunc) {
+                xrmvar.Page.data.refresh(save).then(successFunc, errorFunc);
+            },
+            save: function (successFunc, errorFunc) {
+                xrmvar.Page.data.save().then(successFunc, errorFunc);
+            },
+            saveAndClose: function () {
+                xrmvar.Page.data.entity.save('saveandclose');
+            },
+            saveAndNew: function () {
+                xrmvar.Page.data.entity.save('saveandnew');
+            }
+        },
+        notificatons: {
+            set: function (msg, level, id) {
+                xrmvar.Page.ui.setFormNotification(msg, level, id);
+            },
+            clear: function (id) {
+                xrmvar.Page.ui.clearFormNotification(id);
+            },
+            fieldSet: function (fieldName, msg, id) {
+                var control = crmjs.attr.getControl(fieldName);
+                control.setNotification(msg, id);
+            },
+            fieldClear: function (fieldName, id) {
+                var control = crmjs.attr.getControl(fieldName);
+                control.clearNotification(id);
+            }
+        },
+        iframe: {
+            getSrc: function (iFrameName) {
+                var control = crmjs.attr.getControl(iFrameName);
+                return control.getSrc();
+            },
+            setSrc: function (iFrameName, src) {
+                var control = crmjs.attr.getControl(iFrameName);
+                return control.setSrc(src);
+            },
+            get: function (iframeName) {
+                var control = crmjs.attr.getControl(iFrameName);
+                return control.getObject();
+            }
+        },
+        debug: {
+            log: function (msg) {
+                if (typeof console === 'undefined' || typeof console.log === 'undefined') {
+                    console = {};
+                    console.log = function (msg) {
+                        alert(msg);
+                    };
+                }
+                console.log('debug: ' + msg);
             }
         }
     }
-}
+})();
